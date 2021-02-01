9b297c4162137899b20696759dfe3752d8cfd0a8
alvarodachez <sanchez99esp@gmail.com>
2021-02-01 20:54:02 +0100

    Creacion Repo y DTO User

--- src/main/java/com/jacaranda/repository/DietUserRepository.java
+++ src/main/java/com/jacaranda/repository/DietUserRepository.java
@@ -1,5 +1,14 @@
 package com.jacaranda.repository;
 
-public class DietUserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jacaranda.model.DietUser;
+
+@Repository
+public interface DietUserRepository extends JpaRepository<DietUser, Long> {
+
+	public Optional<DietUser> findByUsername(String username);
 }
