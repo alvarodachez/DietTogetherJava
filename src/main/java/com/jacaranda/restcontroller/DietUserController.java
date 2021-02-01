9d10c0434282e5de1043c24ad6eb7dea4eb1519b
Raúl Morales <raulmdaw@gmail.com>
2021-02-01 21:00:39 +0100

    Añadido Controller y Service

--- src/main/java/com/jacaranda/restcontroller/DietUserController.java
+++ src/main/java/com/jacaranda/restcontroller/DietUserController.java
@@ -1,5 +1,32 @@
 package com.jacaranda.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.jacaranda.model.DietUser;
+import com.jacaranda.model.dto.DietUserDTO;
+import com.jacaranda.services.impl.DietUserServiceImpl;
+
+@RestController
+@RequestMapping("/user")
 public class DietUserController {
 
+	@Autowired
+	private DietUserServiceImpl userService;
+	
+	@PostMapping("/")
+	private ResponseEntity<DietUser> createNewUser(@RequestBody DietUserDTO user) {
+		try {
+			return ResponseEntity.status(HttpStatus.CREATED).body(userService.createNewUser(user));
+		}catch(Exception ex) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
 }
--- src/main/java/com/jacaranda/services/impl/DietUserServiceImpl.java
+++ src/main/java/com/jacaranda/services/impl/DietUserServiceImpl.java
@@ -1,7 +1,31 @@
 package com.jacaranda.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jacaranda.model.DietUser;
+import com.jacaranda.model.dto.DietUserDTO;
+import com.jacaranda.repository.DietUserRepository;
 import com.jacaranda.services.DietUserServiceI;
 
+@Service("userService")
 public class DietUserServiceImpl implements DietUserServiceI{
 
+	@Autowired
+	private DietUserRepository repository;
+	
+	@Autowired
+//	private UserDTOConverter converter;
+
+//	@Override
+//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		return repository.findByUsername(username)
+//				.orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+//	}
+	
+	
+	public DietUser createNewUser(DietUserDTO userDTO) {
+		return null;
+//		return repository.save(converter.fromUserDTOToUser(userDTO)); 
+	}
 }
