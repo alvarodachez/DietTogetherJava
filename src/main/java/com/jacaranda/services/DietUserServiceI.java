d83b066132cab9ddcd0be7d857906e2e6e3b663f
Raúl Morales <raulmdaw@gmail.com>
2021-02-01 21:08:53 +0100

    Corregido ServiceI y ServiceImpl

--- src/main/java/com/jacaranda/services/DietUserServiceI.java
+++ src/main/java/com/jacaranda/services/DietUserServiceI.java
@@ -1,5 +1,13 @@
 package com.jacaranda.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.jacaranda.model.DietUser;
+import com.jacaranda.model.dto.DietUserDTO;
+
 public interface DietUserServiceI {
 
+	public UserDetails loadUserByUsername(String username);
+
+	public DietUser createNewUser(DietUserDTO userDTO);
 }
--- src/main/java/com/jacaranda/services/impl/DietUserServiceImpl.java
+++ src/main/java/com/jacaranda/services/impl/DietUserServiceImpl.java
@@ -1,31 +1,32 @@
 package com.jacaranda.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.jacaranda.model.DietUser;
 import com.jacaranda.model.dto.DietUserDTO;
+import com.jacaranda.model.dto.DietUserDTOConverter;
 import com.jacaranda.repository.DietUserRepository;
 import com.jacaranda.services.DietUserServiceI;
 
 @Service("userService")
-public class DietUserServiceImpl implements DietUserServiceI{
+public class DietUserServiceImpl implements DietUserServiceI {
 
 	@Autowired
 	private DietUserRepository repository;
-	
-	@Autowired
-//	private UserDTOConverter converter;
 
-//	@Override
-//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		return repository.findByUsername(username)
-//				.orElseThrow(()-> new UsernameNotFoundException("Username not found"));
-//	}
-	
-	
+	@Autowired
+	private DietUserDTOConverter converter;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return repository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+	}
+
 	public DietUser createNewUser(DietUserDTO userDTO) {
-		return null;
-//		return repository.save(converter.fromUserDTOToUser(userDTO)); 
+		return repository.save(converter.fromUserDTOToUser(userDTO));
 	}
 }
