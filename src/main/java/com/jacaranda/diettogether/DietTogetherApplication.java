5391c0d735950f7bf0cc416b3d4eca193c9723ae
Raúl Morales <raulmdaw@gmail.com>
2021-03-06 14:03:27 +0100

    Cambios para publicar aplicación en Tomcat

--- src/main/java/com/jacaranda/diettogether/DietTogetherApplication.java
+++ src/main/java/com/jacaranda/diettogether/DietTogetherApplication.java
@@ -3,6 +3,7 @@ package com.jacaranda.diettogether;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = { "com.jacaranda.restcontroller", "com.jacaranda.services",
@@ -11,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		"com.jacaranda.security.model", "com.jacaranda.security.common", "com.jacaranda.security.filter", "com.jacaranda.security.filter.jwt" })
 @EntityScan(basePackages = { "com.jacaranda.model", "com.jacaranda.security.model" })
 @EnableJpaRepositories(basePackages = { "com.jacaranda.repository", "com.jacaranda.security.repository" })
-public class DietTogetherApplication {
+public class DietTogetherApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DietTogetherApplication.class, args);
