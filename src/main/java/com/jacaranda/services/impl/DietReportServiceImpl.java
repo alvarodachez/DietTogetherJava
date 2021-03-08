1d83e68965a7f4b32e3be81bd768f6832cc34841
Alvaro <sanchez99esp@gmail.com>
2021-03-08 17:57:09 +0100

    añadida fecha de creacion a los reportes

--- src/main/java/com/jacaranda/services/impl/DietReportServiceImpl.java
+++ src/main/java/com/jacaranda/services/impl/DietReportServiceImpl.java
@@ -8,45 +8,46 @@ import com.jacaranda.services.DietReportServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service("reportService")
 public class DietReportServiceImpl implements DietReportServiceI {
 
-    @Autowired
-    DietReportRepository reportRepo;
+	@Autowired
+	DietReportRepository reportRepo;
 
-    @Autowired
-    DietUserRepository userRepo;
+	@Autowired
+	DietUserRepository userRepo;
 
+	@Override
+	public List<DietReport> getUserReports(String username) {
 
-    @Override
-    public List<DietReport> getUserReports(String username) {
+		/** Obtenemos usuario por nombre de usuario */
+		DietUser user = userRepo.findByUsername(username).get();
 
-        /** Obtenemos usuario por nombre de usuario */
-        DietUser user = userRepo.findByUsername(username).get();
+		return user.getAthleteId().getReports();
+	}
 
-        return user.getAthleteId().getReports();
-    }
+	@Override
+	public List<DietReport> getAllReports() {
+		return reportRepo.findAll();
+	}
 
-    @Override
-    public List<DietReport> getAllReports() {
-        return reportRepo.findAll();
-    }
+	@Override
+	public DietReport createReport(String username, DietReport report) {
+		/** Obtenemos usuario por nombre de usuario */
+		DietUser user = userRepo.findByUsername(username).get();
 
-    @Override
-    public DietReport createReport(String username, DietReport report) {
-        /** Obtenemos usuario por nombre de usuario */
-        DietUser user = userRepo.findByUsername(username).get();
+		DietReport reportToCreate = new DietReport();
+		reportToCreate.setReportCategory(report.getReportCategory());
+		reportToCreate.setDescription(report.getDescription());
+		reportToCreate.setCreateReportDate(LocalDate.now());
+		reportRepo.save(reportToCreate);
 
-        DietReport reportToCreate = new DietReport();
-        reportToCreate.setReportCategory(report.getReportCategory());
-        reportToCreate.setDescription(report.getDescription());
-        reportRepo.save(reportToCreate);
+		user.getAthleteId().getReports().add(reportToCreate);
+		userRepo.save(user);
 
-        user.getAthleteId().getReports().add(reportToCreate);
-        userRepo.save(user);
-
-        return reportToCreate;
-    }
+		return reportToCreate;
+	}
 }
